ef8053815f6f3fd3231856d9f6eb40ac28dd5159
Ajeet jha <jhaajeetcs@gmail.com>
2022-11-04 11:32:32 +0530

    Added basic router v6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Layout from './components/Layout';
+import About from './components/About';
+import Dashboard from './components/Dashboard';
+import Home from './components/Home';
+import NoMatch from './components/NoMatch';
 
 function App() {
-	return <div className="App">code splitting</div>;
+	return (
+		<div>
+			<h1>Basic Example</h1>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home />} />
+					<Route path="about" element={<About />} />
+					<Route path="dashboard" element={<Dashboard />} />
+					<Route path="*" element={<NoMatch />} />
+				</Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 /* eslint-disable */
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import loadable from '@loadable/component';
-const App = loadable(() =>
-	import(/* webpackChunkName: "loadable-app" */ './App')
-);
+import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<Suspense fallback={<div>Loading </div>}>
+		<BrowserRouter>
 			<App />
-		</Suspense>
+		</BrowserRouter>
 	</React.StrictMode>
 );
